54b369672410a51afee0912ceec3585c67b94e79
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-10 17:09:13 -0600

    finally got sperate func to be invoked in $.getJson

--- scripts/Api.js
+++ scripts/Api.js
@@ -9,20 +9,21 @@ const api = (function(){
       q: searchTerm
     };
     
-    $.getJSON(BASE_URL, query, (response) => {
-      response = response.items.map(item => {
-        return {
-          id: item.id,
-          title: item.snippet.title,
-          thumbnail: item.snippet.thumbnails.medium.url,
-        };
-      });
-      callback(response);
+    $.getJSON(BASE_URL, query, (response) => { //get json and run an anon function that has parameter of response
+      callback(test(response));// run the callback with the test method and response parameter
     });
   };
-
   
-
+  const test = function (response){
+    response = response.items.map(item => {
+      return {
+        id: item.id,
+        title: item.snippet.title,
+        thumbnail: item.snippet.thumbnails.medium.url,
+      };
+    });
+    return response;
+  };
   return {
     fetchVideos,
   };
